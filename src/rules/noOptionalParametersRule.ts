c7082ec4651f7f3d28a536511be6164238550d8b
Nick Westman <n2westman@microsoft.com>
2015-05-25 14:52:06 -0700

    Updated Failure String

--- src/rules/noOptionalParametersRule.ts
+++ src/rules/noOptionalParametersRule.ts
@@ -1,5 +1,5 @@
 export class Rule extends Lint.Rules.AbstractRule {
-    public static FAILURE_STRING = "optional parameters forbidden";
+    public static FAILURE_STRING = "optional parameters forbidden: ";
 
     public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
         return this.applyWithWalker(new NoOptionalParametersWalker(sourceFile, this.getOptions()));
@@ -10,7 +10,7 @@ class NoOptionalParametersWalker extends Lint.RuleWalker {
     public visitPropertySignature(node) {
         // create a failure at the current position
         if (node.questionToken !== undefined) {
-            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING + node.name.text));
         }
         // call the base version of this visitor to actually parse this node
         super.visitPropertySignature(node);
