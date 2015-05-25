d436bc7349b472eb7ee8006a37776a040a7c61c5
Nick Westman <n2westman@microsoft.com>
2015-05-25 14:08:03 -0700

    New Rule for Optional Parameters

---
+++ src/rules/noOptionalParametersRule.ts
@@ -0,0 +1,18 @@
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "optional parameters forbidden";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoOptionalParametersWalker(sourceFile, this.getOptions()));
+    }
+}
+
+class NoOptionalParametersWalker extends Lint.RuleWalker {
+    public visitPropertySignature(node) {
+        // create a failure at the current position
+        if (node.questionToken !== undefined) {
+            this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING));
+        }
+        // call the base version of this visitor to actually parse this node
+        super.visitPropertySignature(node);
+    }
+}
